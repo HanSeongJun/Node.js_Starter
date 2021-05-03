b8421be5871400f085057f8f48a08ba5ff1a3099
HanSeoungJun <67749318+HanSeongJun@users.noreply.github.com>
2021-05-03 20:00:54 +0900

    Chapter.9_1

---
+++ Chapter.9/nodebird/models/hashtag.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Hashtag extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+      }
+    );
+  }
+};
